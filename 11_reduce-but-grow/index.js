3d1fc0e0341e764c3ebb6b81c36b1f6b5060ba74
katmart93 <katwlodarczyk93@gmail.com>
2022-07-27 19:59:39 +0200

    exercise 11 solved

--- 11_reduce-but-grow/index.js
+++ 11_reduce-but-grow/index.js
@@ -2,4 +2,22 @@
 
 //[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
 
-function grow(x) {}
+// #1 solution
+const arr = [4, 1, 1, 1, 4];
+
+let result = 1;
+function grow(x) {
+  result = 1;
+  for (let i = 0; i < x.length; i++) {
+    result = result * x[i];
+  }
+  return result;
+}
+
+console.log("From solution #1", grow(arr));
+
+// #2 solution
+const arr2 = [1, 2, 3, 4, 5];
+const grow2 = (x) => x.reduce((prev, curr) => prev * curr);
+
+console.log("From solution #2", grow2(arr2));
