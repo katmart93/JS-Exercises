7466ec9819976ff4948cebe4f443746d4f624a47
katmart93 <katwlodarczyk93@gmail.com>
2022-10-08 14:41:47 +0200

    exercise 18

---
+++ 18-grasshopper-summation/index.js
@@ -0,0 +1,21 @@
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+// For example:
+
+// summation(2) -> 3
+// 1 + 2
+
+// summation(8) -> 36
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+var summation = function (num) {
+  let result = 0;
+  for (let i = 1; i <= num; i++) {
+    result += i;
+  }
+  return result;
+};
+console.log(summation(8));
+
+// #2
+const summation2 = (n) => (n * (n + 1)) / 2;
